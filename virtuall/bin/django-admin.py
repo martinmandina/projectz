ac89b42df017dd585e037903c154f547cc52eea1
martinsmandina <martinsmandina@gmail.com>
2020-11-30 12:35:02 +0300

    change virtual

--- virtuall/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/martinmandina/Documents/Core-Moringa/Python/Django/Projects/ProjectsApp/virtuall/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
