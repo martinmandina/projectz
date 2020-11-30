e9eff6b6cc46ee0b55ef0f6061fae5de2c12c623
martinsmandina <martinsmandina@gmail.com>
2020-11-30 14:16:42 +0300

    create main view

--- ProjectApp/views.py
+++ ProjectApp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Project,Profile
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
+
+def main(request):
+    projects = Project.objects.all()
+    return render(request, 'index.html',{'projects':projects})
+
+
+
